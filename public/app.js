ca5779420c990d03f94949171adcaa6d47deab85
mehmet <mehmetcimen23@gmail.com>
2021-01-08 11:18:19 +0000

    reading the dom

--- public/app.js
+++ public/app.js
@@ -1,3 +1,10 @@
 "use strict";
-var anchor = document.querySelector('a');
-console.log(anchor.href);
+var form = document.querySelector('.new-item-form');
+var type = document.querySelector('#type');
+var tofrom = document.querySelector('#tofrom');
+var details = document.querySelector('#details');
+var amount = document.querySelector('#amount');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
