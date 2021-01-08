f61ca1b11ee891108f754851475d6ff0e26ae526
mehmet <mehmetcimen23@gmail.com>
2021-01-08 11:46:20 +0000

    using modules

--- public/app.js
+++ public/app.js
@@ -1,10 +1,14 @@
-"use strict";
-var form = document.querySelector('.new-item-form');
-var type = document.querySelector('#type');
-var tofrom = document.querySelector('#tofrom');
-var details = document.querySelector('#details');
-var amount = document.querySelector('#amount');
-form.addEventListener('submit', function (e) {
+import { Invoice } from './classes/Invoice.js';
+const invOne = new Invoice('mario', 'work on mario website', 30);
+const invTwo = new Invoice('luigi', 'work on luigi website', 300);
+let invoices = [invOne, invTwo];
+console.log(invoices);
+const form = document.querySelector('.new-item-form');
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
+import { Invoice } from './classes/Invoice.js';
+
+const invOne = new Invoice('mario', 'work on mario website', 30);
+const invTwo = new Invoice('luigi', 'work on luigi website', 300);
+
+let invoices: Invoice[] = [invOne, invTwo];
+
+console.log(invoices);
+
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
 
