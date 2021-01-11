30c32e68ba01f0dd321872c259cded151d8c18b8
mehmet <mehmetcimen23@gmail.com>
2021-01-11 13:37:10 +0000

    updates

--- public/app.js
+++ public/app.js
@@ -1,25 +1,24 @@
-const me = {
-    name: 'shaun',
-    age: 30,
-    speak(text) {
-        console.log(text);
-    },
-    spend(amount) {
-        console.log(`I spent ${amount}`);
-        return amount;
-    }
-};
 import { Invoice } from './classes/Invoice.js';
-const invOne = new Invoice('mario', 'work on mario website', 30);
-const invTwo = new Invoice('luigi', 'work on luigi website', 300);
-let invoices = [invOne, invTwo];
-console.log(invoices);
+import { Payment } from './classes/Payment.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 const form = document.querySelector('.new-item-form');
+console.log(form.children);
+// inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, 'end');
 });
--- src/interfaces/HasFormatter.ts
+++ src/interfaces/HasFormatter.ts
@@ -1,4 +1,3 @@
 export interface HasFormatter {
     format(): string;
-
 }
